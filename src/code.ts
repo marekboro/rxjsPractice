d1369b5ab674d69ee2576a9069c47df766b9f890
marekboro <marek.borowicz83@gmail.com>
2021-02-20 15:50:54 +0000

    cold, warm and hot observables

--- src/code.ts
+++ src/code.ts
@@ -1,11 +1,16 @@
-import { Observable } from 'rxjs'
+import { from, Observable } from 'rxjs'
+import { share } from 'rxjs/operators'
+import { fromEvent } from 'rxjs'
+
 
 // var observable = new Observable(function subscribe(observer:any){
 //     observer.next('Hey there, im an emition from an observable being observed')  // EMITTING A VALUE! use observer.next. 
 // } );
-var observable = new Observable((observer: any) => {
+var observable = new Observable((observer: any) => {    // This is a COLD OBSERVABLE - its producer is the function  in YELLOW () brackets, as this is what emitts these values/events.
+                                                        // "its observer whose producer is activated only once a subscription has been created" 
+                                                        // observable is HOT when the producer is emitting  value outside of the ebservable.
     try {
-        observer.next('Hey there, im an emition from an observable being observed');  // EMITTING A VALUE! use observer.next. 
+        observer.next('Hey there, im an emission from an observable being observed');  // EMITTING A VALUE! use observer.next. 
         observer.next('How are you');  // EMITTING A VALUE! use observer.next. 
         setInterval( ()=> {observer.next('I am OK')}, 2000 )
         // observer.complete();
@@ -13,32 +18,55 @@ var observable = new Observable((observer: any) => {
     }
     catch (err) {
         observer.error(err);
-
     }
-});
 
-// observable.subscribe((x:any) => addItem(x)) // everytime you subscribe you create an OBSERVER, you create a subscription. 
+//! SWITCH which line is commented out to make the Subscriber2 a WARM / HOT observable. 
+});
+// }).pipe(share()) ;
+//! TRULY HOT observbls would be MOUSE movememts.
+
+
+// observable.subscribe((x:any) => addItemOld(x)) // everytime you subscribe you create an OBSERVER, you create a subscription. 
 // the observers read valuse coming from the observable. 
 // obseervers are a set of callbacks that accept notifications coming from the observable: next,error,complete 
 var observer = observable.subscribe(
-    (x: any) => addItem(x)
+    (x: any) => addItemOld(x)
     
 )
 
-
-var observer2 = observable.subscribe(
-    (x: any) => addItem(x),
-    (error: any) => addItem(error),
-    () => addItem("Completed")
-)
-
-observer.add(observer2); // this will cause 'observer2' to get unsubscribed when we unsubscribe from 'observer', observer2 becomes a CHILD SUBSCRIPTION. 
-
+////
+// var observer2 = observable.subscribe(
+//     (x: any) => addItemOld(x),
+//     (error: any) => addItemOld(error),
+//     () => addItemOld("Completed")
+// )
+// observer.add(observer2); // this will cause 'observer2' to get unsubscribed when we unsubscribe from 'observer', observer2 becomes a CHILD SUBSCRIPTION. 
+// setTimeout( () => {
+//   observer.unsubscribe()
+// },6005)
+////
 setTimeout( () => {
-  observer.unsubscribe()
-},6005)
+  var observer2 = observable.subscribe(
+      (x:any)=>addItemOld('Subscriber 2 '+ x)
+  )
+},1000)
 
+ 
 
+function addItemOld(val: any) {
+    // var node = document.createElement("li");
+    // var textnode = document.createTextNode(val);
+    // node.appendChild(textnode);
+    // document.getElementById("output").appendChild(node);
+}
+
+var observableHot = fromEvent(document,'mousemove')
+
+setTimeout(() => {
+  var subscription = observableHot.subscribe(
+      (x:any)=> addItem(x)
+  )
+},2000)
 
 function addItem(val: any) {
     var node = document.createElement("li");
